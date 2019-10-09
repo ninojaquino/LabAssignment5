588879dd35381909a5f829e7da0a861887880cff
ninojaquino <ninojaquino@gmail.com>
2019-10-08 23:07:10 -0700

    removing table and button

--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -1,11 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 
-interface IStudent {
-  id?: number;
-  firstName: string;
-  lastName: string;
-  course: string;
-}
 
 @Component({
   selector: 'main-content',
@@ -14,49 +8,12 @@ interface IStudent {
 })
 export class MainContentComponent implements OnInit {
 
-  students: Array<IStudent> = [];
+  
 
-  constructor() {
-    this.students[0] = {
-      id: 1,
-      firstName: 'Nino',
-      lastName: 'Aquino',
-      course: 'Programming'
-    }
-    this.students[1] = {
-      id: 2,
-      firstName: 'LeBron',
-      lastName: 'James',
-      course: 'Nursing'
-    }
-    this.students[2] = {
-      id: 3,
-      firstName: 'Kobe',
-      lastName: 'Bryant',
-      course: 'Art'
-    }
-    this.students[3] = {
-      id: 4,
-      firstName: 'Michael',
-      lastName: 'Jordan',
-      course: 'Culinary'
-    }
-  }
+  constructor() {}
 
 
   ngOnInit() {
-  }
-
-  addStudent() {
-    const student: IStudent = {
-      id: 5,
-      firstName: 'Dwyane',
-      lastName: 'Wade',
-      course: 'Military'
-    };
-
-    this.students.push(student);
-
   }
 
 }
